613fdfacbcb7973169f1440ccf06d250ab7d0435
nguyendangvu221 <nguyendangvu221@gmail.com>
2024-02-21 16:49:47 +0700

    fix  feature count vietnamese letters

--- main.dart
+++ main.dart
@@ -1,15 +1,20 @@
 import 'dart:io';
+/*
+Cách làm này sử dụng một mảng vietnameseChars chứa tất cả các cặp ký tự Tiếng Việt có thể xuất hiện trong chuỗi. Sau đó, trong vòng lặp, chúng ta kiểm tra từng ký tự trong chuỗi đầu vào.
+Nếu ký tự hiện tại kết hợp với ký tự tiếp theo tạo thành một cặp ký tự Tiếng Việt, ta tăng biến đếm count, thêm cặp ký tự đó vào danh sách foundChars, và bỏ qua ký tự tiếp theo vì đã tạo thành cặp.
+Nếu ký tự hiện tại không tạo thành cặp với ký tự tiếp theo, ta kiểm tra xem ký tự đó có phải là một ký tự Tiếng Việt không. Nếu có, ta cũng tăng biến đếm count và thêm ký tự đó vào danh sách foundChars.
+Cuối cùng, sau khi kiểm tra hết chuỗi đầu vào, chúng ta in ra số lượng và danh sách các ký tự Tiếng Việt tìm thấy tương ứng. */
 
 void countAndPrintVietnameseChars(String str) {
-  final vietnameseChars = {
-    'aa': 'aa',
-    'aw': 'aw',
-    'dd': 'dd',
-    'ee': 'ee',
-    'oo': 'oo',
-    'ow': 'ow',
-    'w': 'w',
-  };
+  final vietnameseChars = [
+    'aa',
+    'aw',
+    'dd',
+    'ee',
+    'oo',
+    'ow',
+    'w',
+  ];
 
   int count = 0;
   List<String> foundChars = [];
@@ -17,15 +22,15 @@ void countAndPrintVietnameseChars(String str) {
     String currentChar = str[i];
     if (i < str.length - 1) {
       String charPair = str.substring(i, i + 2);
-      if (vietnameseChars.containsKey(charPair)) {
+      if (vietnameseChars.contains(charPair)) {
         count++;
-        foundChars.add(vietnameseChars[charPair]!);
+        foundChars.add(charPair);
         i++;
       }
     }
-    if (vietnameseChars.containsKey(currentChar)) {
+    if (vietnameseChars.contains(currentChar)) {
       count++;
-      foundChars.add(vietnameseChars[currentChar] ?? '');
+      foundChars.add(currentChar);
     }
   }
 
